5d7c15834245b49a267bc5817ff2bc4214c8f1df
tcalvk <89819712+kleintan000@users.noreply.github.com>
2022-04-30 23:34:00 -0600

    Initial Commit

---
+++ create_mailing_list.py
@@ -0,0 +1,62 @@
+import itertools
+import csv
+import os
+import getpass
+import pandas as pd
+import requests
+import io
+
+
+def check_for_updates(current_version):
+    url = "https://raw.githubusercontent.com/tcalvk/Create_Mailing_List/main/version.csv" # Make sure the url is the raw version of the file on GitHub
+    download = requests.get(url).content
+    df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+    new_version_raw = df.to_csv()
+    new_version_str = new_version_raw[1:]
+    new_version_num = float(new_version_str)
+    if new_version_num > current_version:
+        return 'yes'
+    else: 
+        return None
+
+
+def create_list():
+    user = getpass.getuser()
+    directory = f'/Users/{user}/downloads/Mailing_Lists'
+    filename = f'/Users/{user}/downloads/Mailing_Lists/create_list.csv'
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+        with open(f'/Users/{user}/downloads/Mailing_Lists/create_list.csv', "w") as filename:
+            print()
+    else:    
+        if not os.path.exists(filename):
+            with open(f'/Users/{user}/downloads/Mailing_Lists/create_list.csv', "w") as filename:
+                print()
+        else: 
+            print()
+    
+    try: 
+        directory_out = f'/Users/{user}/downloads/Mailing_Lists'
+        filename_out = 'list.csv'
+        file_in = os.path.join(directory, filename)
+        file_out = os.path.join(directory_out, filename_out)
+        file_temp = f'/Users/{user}/downloads/Mailing_Lists/_temp.csv'
+
+        with open(file_in, "r", newline='') as f_input, \
+                open(file_out, "a", newline='') as f_output, \
+                open(file_temp, "w", newline='') as f_temp:
+            
+            csv_input = csv.reader(f_input)
+
+            # Append first 50 rows to file_out
+            csv.writer(f_output).writerows(itertools.islice(csv_input, 0, 500))
+
+            # Write the remaining rows from file_in to file_temp
+            csv.writer(f_temp).writerows(csv_input)
+
+        # Rename f_temp to file_in, first remove existing file_in then rename temp file
+        os.remove(file_in)
+        os.rename(file_temp, file_in)
+        return 'yes'
+    except Exception:
+        return 'no'
---
+++ ui.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import tkinter as tk
+import create_mailing_list as cm
+import os
+from tkinter import messagebox 
+import getpass
+
+#place current version number here (as global variable)
+current_version = float(0.0)
+appName = 'Email List App'
+USER = getpass.getuser()
+
+def build_window():
+    window = tk.Tk()
+    window.title("Create Mailing List")
+    window.geometry("300x150")
+    create_buttons()  
+    window.mainloop()
+
+def create_buttons():
+    button1 = tk.Button(text="Click to run", command=run)
+    button1.pack()
+
+def run():
+    verify = cm.create_list()
+    if verify.lower() == 'yes':
+        messagebox.showinfo("Info", "A new list was created")
+    else:
+        messagebox.showerror("Error", "Error: A list could not be created.")
+
+def load_updates(answer):
+    if answer == 'yes':
+        #display a messagebox with the option to install updates
+        msgBox = messagebox.askquestion(f"{appName}", "Updates are ready. Would you like to install now?")
+        if msgBox == 'yes':
+            return 'yes'
+        elif msgBox == 'no': 
+            return None 
+        else: 
+            return None 
+    else: 
+        return None
+
+def main():
+    answer = cm.check_for_updates(current_version)
+    update = load_updates(answer)
+    if update == 'yes':
+        print()
+        os.system('open "/Applications/DO NOT DELETE/Corsaire_update_installer"')
+    elif update == None:
+        build_window()
+
+        
+
+if __name__ == "__main__":
+    main()
+
+
